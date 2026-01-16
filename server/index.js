68ae036077f4b2841b8f1e4f039320db0f6ab157
CharliePeers <63914466+CharliePeers@users.noreply.github.com>
2026-01-15 22:31:48 -0500

    set up express API with basic member endpoints. Server running on port 3000

--- server/index.js
+++ server/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const app = express();  
 const cors = require('cors');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
+const members = require('./dali_social_media.json');
 
-// Middleware
+//middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
-app.get('/', (req, res) => {
-  res.send('DALI Social Media API is Running!');
+//get all dali members
+app.get('/api/members', (req, res) => {
+  res.json(members);
 });
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//get one member name
+app.get('/api/members/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id >= 0 && id < members.length) {
+    res.json(members[id]);
+  } else {
+    res.status(404).json({ message: 'Member not found.' });
+  }
 });
+
+app.get('/api/members/role/:role')
+
+//start Server
+app.listen(
+  PORT,
+  () => { console.log(`it's live on http://localhost:${PORT}`);
+}
+);
