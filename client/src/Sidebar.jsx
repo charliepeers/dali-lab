a97e700650e87f70bbcc153f01782e5390f0a90e
CharliePeers <63914466+CharliePeers@users.noreply.github.com>
2026-01-20 22:29:44 -0500

    Sidebar icons and remove Peep button
    Removed the Peep button and its related CSS and adjusted Sidebar sections to use new icons

--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import './Sidebar.css'
-import { Birdhouse, Hash, Bell } from "lucide-react";
+import { PiBird} from "react-icons/pi"; //only load icons that I need = improves page speed 
+import { GiNestBirds } from "react-icons/gi";
+import { HiOutlineHashtag } from "react-icons/hi";
 import SidebarSec from "./SidebarSec.jsx";
 
 function Sidebar(){
   return(
   <div className="sidebar">
-    <SidebarSec active Icon={Birdhouse} text="Home"/>
-    <SidebarSec Icon={Hash}  text="Explore"/>
-    <SidebarSec Icon={Bell} text="Notifications"/>
+    <div className="sidebar__logo">
+      <PiBird /><span> Dalibird</span></div>
 
-    <button className="peepButton"><span>Peep</span></button>
+    <SidebarSec active Icon={GiNestBirds} text="Home"/>
+    <SidebarSec Icon={HiOutlineHashtag} text="Explore"/>
   </div>
 );
 }
