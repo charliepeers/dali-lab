444632c5e167d26e0225f93e1c851abecd47ef42
CharliePeers <63914466+CharliePeers@users.noreply.github.com>
2026-01-23 11:12:18 -0500

    Add controlled input and submit handler to Peepbox

--- client/src/Peepbox.jsx
+++ client/src/Peepbox.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react'; //import to track what user types
 import './Peepbox.css';
 
-function Peepbox() {
+function Peepbox({ onCreatePost }) {
+  const [peepText, setPeepText] = useState('');
+
+  //so page doesn't refresh
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  if (peepText.trim()) {
+      onCreatePost(peepText); //sends to backend
+      setPeepText('');  //clears input after posting
+    }
+  };
+
   return (
     <div className="peepBox">
-      <form className="form">
-        <input className="peepBoxInput" placeholder="What's on your mind?" />
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="peepBoxInput"
+          placeholder="What's on your mind?"
+          value={peepText}
+          onChange={(e) => setPeepText(e.target.value)}/>
+
         <button className="peepBox__peepButton"><span>Peep</span></button>
       </form>
     </div>
